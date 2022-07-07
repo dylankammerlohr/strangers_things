843d097580d2e3f823f76c75b713419b03f1f6a3
Dylan Kammerlohr <dylankammerlohr@gmail.com>
2022-07-07 11:07:04 -0500

    post view done

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -4,8 +4,18 @@ import React from 'react'
 const UserPost = ({allPosts}) => {
    console.log(allPosts, 'all posts in post comp')
     return(
-        <div className="posts">
-            <h2>{}</h2>
+        <div className="postss">
+            {allPosts.map((post) => {
+                return (
+                    <div>
+                        <h2>{post.title}</h2>
+                        <p>{post.description}</p>
+                        <h4>{post.price}</h4>
+                        <h3>Seller: {post.author.username}</h3>
+                        <h4>{post.location}</h4>
+                    </div>
+                )
+            })}
         </div>
     )
 
